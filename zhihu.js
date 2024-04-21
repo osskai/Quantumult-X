a6ab71e421fba03d878720a9aec5c683f5ca3799
osskai <56211917+osskai@users.noreply.github.com>
2024-04-21 12:08:55 +0800

    Update zhihu.js

--- zhihu.js
+++ zhihu.js
@@ -1,1 +1,17 @@
+if (!$response.body) $done({});
+const url = $request.url;
+let obj = JSON.parse($response.body);
 
+if (url.includes("/api/v4/answers")) {
+  if (obj?.data) {
+    delete obj.data;
+  }
+  if (obj?.paging) {
+    delete obj.paging;
+  }
+} else if (url.includes("/api/v4/articles")) {
+  const item = ["ad_info", "paging", "recommend_info"];
+  item.forEach((i) => {
+    delete obj[i];
+  });
+}
