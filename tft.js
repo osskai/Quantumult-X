d8dee4127ad111f3ca7ccc7587d7c9da05c24779
osskai <56211917+osskai@users.noreply.github.com>
2024-04-21 21:05:55 +0800

    Update tft.js

--- tft.js
+++ tft.js
@@ -5,19 +5,19 @@ let obj = JSON.parse($response.body);
 if (url.includes("/prod")) {
   const deletedItems = ['出行', '财富']
   for (var i = 0; i < 2; ++i) {
-      const existedItems = source[i]['navigationAll']
+      const existedItems = obj[i]['navigationAll']
       const output = existedItems.filter((item) => {
           const itemName = item['lableName']
           return deletedItems.indexOf(itemName) === -1
       })
-      source[i]['navigationAll'] = output
+      obj[i]['navigationAll'] = output
 
-      const conentItems = source[i]['content']
+      const conentItems = obj[i]['content']
       const output2 = conentItems.filter((item) => {
           const sortIndex = parseInt(item['sort'])
           return sortIndex < 2
       })
-      source[i]['content'] = output2
+      obj[i]['content'] = output2
   }
 } 
 
