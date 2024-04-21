0f928a566cc8036501d354a688fab106e21586b0
osskai <56211917+osskai@users.noreply.github.com>
2024-04-21 18:29:08 +0800

    Update tft.js

--- tft.js
+++ tft.js
@@ -3,4 +3,15 @@ const url = $request.url;
 let obj = JSON.parse($response.body);
 
 if (url.includes("/prod")) {
+  const existedItems = source[0]['navigationAll']
+  const deletedItems = ['出行', '财富']
+  
+  const length = existedItems.length
+  for (var i = 0; i < length; ++i) {
+    var item = existedItems[i]
+    const itemName = item['lableName']
+    if (deletedItems.indexOf(itemName) !== -1) {
+      delete existedItems[i]
+    } 
+  }
 } 
